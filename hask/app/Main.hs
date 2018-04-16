458a2c359ee2cd37400c4737b6676ee89a3d7b4c
David Noll <david.m.noll@gmail.com>
2018-04-16 04:14:34 -0400

    problem 5 optimized

--- hask/app/Main.hs
+++ hask/app/Main.hs
@@ -1,7 +1,16 @@
 module Main where
 
 import Prob4
+import Prob5
 
 main :: IO ()
 main = do
+    putStrLn "Prob4"
     putStrLn (show (palFromDigits 3))
+
+    putStrLn "Prob5"
+    -- WORKS BUT VERY SLOW
+    -- putStrLn (show (smallestMultiple 20))
+    putStrLn "Prob5 -opt"
+    -- MUCH FASTER
+    putStrLn (show (sMultiple 20))
