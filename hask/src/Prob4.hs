85118b9fe61b8c65d3323f240d8598e1fa185ab2
David Noll <david.m.noll@gmail.com>
2018-04-16 00:30:22 -0400

    problem 4 fixed gitignore issuue  Please enter the commit message for your changes. Lines starting

---
+++ hask/src/Prob4.hs
@@ -0,0 +1,14 @@
+module Prob4 where
+
+palFromDigits :: Int -> Int
+palFromDigits x = biggestPalFromNums (maxOfNrDigits) (maxOfNrDigits)
+    where
+        maxOfNrDigits = ((10 ^ x) - 1)
+
+biggestPalFromNums :: Int -> Int -> Int
+biggestPalFromNums x y =
+    maximum [ i * j | i <- [0..x],  j <-[0..y], isPal (show ( i * j ))]
+    where
+        isPal [] = True
+        isPal (x : []) = True
+        isPal (x : xs) = (x == (last xs) ) && isPal (init xs )
