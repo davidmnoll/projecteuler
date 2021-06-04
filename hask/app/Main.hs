8b014f1900c735cf8c8604d54d2c2e86006e5197
David Noll <david.m.noll@gmail.com>
2021-06-04 10:49:38 -0700

    wip

--- hask/app/Main.hs
+++ hask/app/Main.hs
@@ -2,15 +2,22 @@ module Main where
 
 import Prob4
 import Prob5
+import Prob6
 
 main :: IO ()
 main = do
     putStrLn "Prob4"
+    putStrLn "==="
     putStrLn (show (palFromDigits 3))
 
     putStrLn "Prob5"
+    putStrLn "==="
     -- WORKS BUT VERY SLOW
     -- putStrLn (show (smallestMultiple 20))
     putStrLn "Prob5 -opt"
+    putStrLn "==="
     -- MUCH FASTER
     putStrLn (show (sMultiple 20))
+    putStrLn "Prob6"
+    putStrLn "==="
+    putStrLn (show (nthPrime 10001))
