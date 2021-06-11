e7c1761b463a9edea6c589e77280a0d7feb3f0cb
David Noll <david.m.noll@gmail.com>
2021-06-11 14:36:40 -0700

    wip

--- hask/app/Main.hs
+++ hask/app/Main.hs
@@ -4,9 +4,10 @@ import Control.Exception
 import System.Environment
 import Prob4 ( palFromDigits )
 import Prob5 ( sMultiple )
-import Prob6 (sqDiff)
+import Prob6 ( sqDiff )
 import Prob7 ( nthPrime )
 import Prob8 ( adjacentDigitsMaxProd )
+import Prob9 ( pyProd ) 
 
 
 
@@ -14,12 +15,13 @@ main :: IO ()
 main = do
     args <- getArgs
     case parseArgs args of
-        Nothing -> doOutput "Prob6" (sqDiff 100)
+        Nothing -> doOutput "Prob9" (pyProd 1000)
         Just (4) -> doOutput "Prob4" (palFromDigits 3)
         Just (5) -> doOutput "Prob5" (sMultiple 20)
         Just (7) -> doOutput "Prob7" (nthPrime 10001) 
         Just (6) -> doOutput "Prob6" (sqDiff 100)
         Just (8) -> doMOutput "Prob8" ( adjacentDigitsMaxProd 13)
+        Just (9) -> doOutput "Prob9" (pyProd 1000)
         Just x  
             | x > 0 -> error "Problem not implemented"
             | otherwise -> error "Problem not valid"
